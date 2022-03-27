cb35343b19ec4c8aa165a8f6fbb786adf239c181
Mbulelo Pani <mbulelopani09@gmail.com>
2022-03-27 18:02:38 +0200

    new project

--- src/components/mywork.js
+++ src/components/mywork.js
@@ -4,7 +4,7 @@ import 'aos/dist/aos.css';
 import Aos from 'aos';
 import Gitimage from '../images/tvstop.png'
 import Visitour from '../images/visitour.png'
-import Contactkeeper from '../images/contactkeeper.jpg'
+import Gitgetter from '../images/gitgetter.png'
 import Portfolio from '../images/portfolio.png'
 import Pylot from '../images/pytonlot.jpg'
 
@@ -24,7 +24,7 @@ export class Mywork extends Component {
                     <br/>
                     <div className="row" data-aos="zoom-in">
                         <div className="col-md-4">
-                            <div className="containerI" style={{width: "85%", color: 'white'}}>
+                            <div className="containerI" style={{width: "85%", color: 'white', minHeight:'250px'}}>
                                 <div>
                                 <img src={Gitimage} alt="Avatar" className="image" />
                                 </div>
@@ -49,7 +49,7 @@ export class Mywork extends Component {
                             </div>
                         </div>
                         <div className="col-md-4">
-                            <div className="containerI" style={{width: "85%", color: 'white'}}>
+                            <div className="containerI" style={{width: "85%", color: 'white', minHeight:'250px'}}>
                                 <div>
                                     <img src={Visitour} alt="Avatar" className="image" style={{minHeight: '250px'}}/>
                                 </div>
@@ -72,30 +72,36 @@ export class Mywork extends Component {
                             </div>
                         </div>
                         <div className="col-md-4">
-                            <div className="containerI" style={{width: "85%", color: 'white'}}>
+                            <div className="containerI" style={{width: "85%", color: 'white', minHeight:'250px'}}>
                                 <div>
-                                    <img src={Contactkeeper} alt="Avatar" className="image" />
+                                    <img src={Gitgetter} alt="Avatar" className="image" style={{minHeight: '250px'}}/>
                                 </div>
                                 <div className="overlay">
                                     <div className="container text-center">
                                         <br/><br/>
-                                        <h3>Contact Keeper</h3>
-                                        <h5>Manages and records a registered user's contacts.  </h5>
+                                        <h3>GitGetter</h3>
+                                        <h5>Search for other github users</h5>
                                         <br/>
                                         <div>
                                             <btn style={{margin: '2px'}}>
-                                                <a href="https://github.com/Mbulelo20/Contacts-Keeper.git" rel="noopener noreferrer" target="_blank" className="btn btn-primary ">GitHub link</a>                        
+                                                <a href="https://github.com/Mbulelo20/profile.git" rel="noopener noreferrer" target="_blank" className="btn btn-primary ">GitHub link</a>                        
+                                            </btn>
+                                        </div>
+                                        <div>
+                                            <btn style={{margin: '2px'}}>
+                                                <a href="https://gitgetter.netlify.app/" rel="noopener noreferrer" target="_blank" className="btn btn-primary ">Demo</a>                        
                                             </btn>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        
                     </div>
                     <br/>
                     <div className="row" data-aos="zoom-in">
                         <div className="col-md-4">
-                            <div className="containerI" style={{width: "85%", color: 'white'}}>
+                            <div className="containerI" style={{width: "85%", color: 'white', minHeight:'250px'}}>
                                 <div>
                                 <img src={Portfolio} alt="Avatar" className="image"  style={{minHeight:'250px'}}/>
                                 </div>
@@ -120,7 +126,7 @@ export class Mywork extends Component {
                             </div>
                         </div>
                         <div className="col-md-4">
-                            <div className="containerI" style={{width: "85%", color: 'white'}}>
+                            <div className="containerI" style={{width: "85%", color: 'white', minHeight:'250px'}}>
                                 <div>
                                     <img src="https://www.enca.com/sites/default/files/1388612_58676202.jpg" alt="Avatar" className="image" />
                                 </div>
@@ -145,7 +151,7 @@ export class Mywork extends Component {
                             </div>
                         </div>
                         <div className="col-md-4">
-                            <div className="containerI" style={{width: "85%", color: 'white'}}>
+                            <div className="containerI" style={{width: "85%", color: 'white', minHeight:'250px'}}>
                                 <div>
                                     <img src={Pylot} alt="Avatar" className="image" />
                                 </div>
