e78f1d7baf4872bbca31d05a509ccb12ed6a58fe
Mbulelo Pani <mbulelopani09@gmail.com>
2022-04-30 11:25:17 +0200

    cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-=======
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
@@ -13,6 +5,5 @@ import App from './App';
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
->>>>>>> 01220b31a9f49dc346f02226a3be0d429eb3835d
   expect(linkElement).toBeInTheDocument();
 });
